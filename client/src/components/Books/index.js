feba1387cbf089fff55ca142f8c6d728ec9c0404
binayaluitel2 <binayababuluitel@gmail.com>
2021-04-26 00:19:28 -0400

    Added search functionality, post book route, handleSave function, props/hooks for book divs, search results, etc.

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,29 +1,39 @@
 import React from "react";
 import "./style.css";
 
-function Books () {
-    
+function Books(props) {
     return(
-    <div className="bookResult">
-        <div className="row">
-            <div className="col-md-10"><p>Title</p>
-            <p>Author</p>
+    props.booky.map(function (boox) {
+      return (
+        <div className="bookResult" key={boox.id}>
+          <div className="row">
+            <div className="col-md-10">
+              <p>{boox.volumeInfo.title}</p>
+              <p>{boox.volumeInfo.authors}</p>
             </div>
             <div className="col-md-2">
-                <button className="btn btn-info">View</button>
-                <button className="btn btn-succes">Save</button>
+              <a href={boox.saleInfo.buyLink}> <button className="btn btn-info">View</button></a>
+              <button className="btn btn-succes"  onClick={() =>props.handleSave({
+                  title: boox.volumeInfo.title,
+                  authors: boox.volumeInfo.authors.join(", "),
+                  description: boox.volumeInfo.description,
+                  image: boox.volumeInfo.imageLinks.thumbnail,
+                  link: boox.saleInfo.buyLink
+                  })}>Save</button>
             </div>
-        </div>
-        <div className="row">
+          </div>
+          <div className="row">
             <div className="col-md-3">
-                <img src="#"/>
+              <img src={boox.volumeInfo.imageLinks ? boox.volumeInfo.imageLinks.thumbnail : "https://i.imgur.com/lrPn7VM.jpeg"} alt={boox.title} />
             </div>
             <div className="col-md-9">
-                <p>Details here</p>
+              <p>{boox.volumeInfo.description}</p>
             </div>
+          </div>
         </div>
-    </div>
-    );
+      );
+    })
+  );
 }
 
 export default Books;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default {
-    getBooks : function (book) {
+    googleSearch : function (book) {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
+    },
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData)
     }
 }
